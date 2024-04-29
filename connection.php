16927f37b8c9123d035fc3ce825f2d18df8d148b
Hamza_Benzaoui <hamza.benzaoui@ensia.edu.dz>
2024-04-29 19:13:44 +0100

    SECURED the create password code

--- connection.php
+++ connection.php
@@ -5,7 +5,7 @@ server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'passManager');
+define('DB_NAME', 'CNS_db');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
--- process_form.php
+++ process_form.php
@@ -1,17 +1,26 @@
 <?php
+//SHow errors
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+// Start the session
 session_start();
 
 require_once "connection.php";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $note = $_POST['note'];
-    $url_name = $_POST['url_name'];
-    $password = $_POST['password'];
+    $note = mysqli_real_escape_string($link, $_POST['note']);
+    $url_name = mysqli_real_escape_string($link, $_POST['url_name']);
+    $password = mysqli_real_escape_string($link, $_POST['password']);
     $user_id = $_SESSION["id"];
 
     // Check if the combination of url_name and user_id already exists
-    $query = "SELECT COUNT(*) AS count FROM passwords_for_users WHERE user_id = $user_id AND url_name = '$url_name'";
-    $result = mysqli_query($link, $query);
+    $query = "SELECT COUNT(*) AS count FROM passwords_for_users WHERE user_id = ? AND url_name = ?";
+    $stmt = mysqli_prepare($link, $query);
+    mysqli_stmt_bind_param($stmt, "is", $user_id, $url_name);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
     $row = mysqli_fetch_assoc($result);
     $count = $row['count'];
 
@@ -20,21 +29,23 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         header("location: create.php");
         exit;
     } else if($result) {
-        // Concatenate user inputs directly into the SQL query (vulnerable to SQL injection)
-        $sql = "INSERT INTO passwords_for_users (user_id, note, url_name, password) VALUES ($user_id, '$note', '$url_name', '$password')";
+        // Use prepared statements to insert data into the database
+        $sql = "INSERT INTO passwords_for_users (user_id, note, url_name, password) VALUES (?, ?, ?, ?)";
+        $stmt = mysqli_prepare($link, $sql);
+        mysqli_stmt_bind_param($stmt, "isss", $user_id, $note, $url_name, $password);
 
         // Execute the SQL query
-        if(mysqli_query($link, $sql)){
+        if(mysqli_stmt_execute($stmt)){
             echo "Records inserted successfully.";
             header("location: search.php");
         } else{
             echo "ERROR: Could not execute query: $sql. " . mysqli_error($link);
         }
     }
-    else{
+}   else{
         echo "ERROR: Could not execute query: $sql. " . mysqli_error($link);
     }
-}
+
 
 mysqli_close($link);
 ?>
